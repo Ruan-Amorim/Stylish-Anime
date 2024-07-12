effd62d31e96c088a0a35a4a998aaae8c9513586
Ruan-Amorim <150949063+Ruan-Amorim@users.noreply.github.com>
2024-07-12 14:51:12 +0000

    implementando novo metodo, para troca de playerAnime.

--- scripts/main.js
+++ scripts/main.js
@@ -19,19 +19,34 @@ function geradorPreviewAnime() {
     }
 };
 
-window.AnimeClick = function AnimeClick(Anime){
+function geradorEspecifico(x) {
+    let li = document.createElement("li");
+    li.id = `anime-${x}`;
+    li.innerHTML = `
+        <h2>${ArrayPreviewAnime[x].nome}</h2>
+        <p>${ArrayPreviewAnime[x].sinopse}</p>`;
+    li.style.backgroundImage = `url("${encodeURI(ArrayPreviewAnime[x].imagemPC)}")`;
+    li.setAttribute('onclick', `AnimeClick(${x})`); 
+
+    ulContainerPlayerAnimes.appendChild(li);
+}
+
+window.AnimeClick = function AnimeClick(anime){
+
+    let nameElement = `anime-${anime}`;
+    let oneElement = window.document.getElementById(nameElement);
+
     // Remove o primeiro elemento e o armazena em uma variável
     let firstElement = ArrayPreviewAnime.shift();
 
     // Adiciona o elemento removido ao final da array
     ArrayPreviewAnime.push(firstElement);
 
-    loading();
-}
 
-function loading() {
-    ulContainerPlayerAnimes.innerHTML = ''
-    geradorPreviewAnime();
+    ulContainerPlayerAnimes.removeChild(oneElement);
+    ulContainerPlayerAnimes.appendChild(oneElement);
     bloco1.style.backgroundImage = `url("${encodeURI(ArrayPreviewAnime[0].imagemPC)}")`;
 }
+
+
 geradorPreviewAnime();
