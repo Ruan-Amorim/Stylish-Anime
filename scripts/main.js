21bb342667f644bbcaf4338d35e0c2537a50f61b
Ruan-Amorim <150949063+Ruan-Amorim@users.noreply.github.com>
2024-07-12 15:39:49 +0000

    voltando a maneria antiga de trocar  playerAnime, porque visualmente é melhor.

--- scripts/main.js
+++ scripts/main.js
@@ -19,34 +19,19 @@ function geradorPreviewAnime() {
     }
 };
 
-function geradorEspecifico(x) {
-    let li = document.createElement("li");
-    li.id = `anime-${x}`;
-    li.innerHTML = `
-        <h2>${ArrayPreviewAnime[x].nome}</h2>
-        <p>${ArrayPreviewAnime[x].sinopse}</p>`;
-    li.style.backgroundImage = `url("${encodeURI(ArrayPreviewAnime[x].imagemPC)}")`;
-    li.setAttribute('onclick', `AnimeClick(${x})`); 
-
-    ulContainerPlayerAnimes.appendChild(li);
-}
-
-window.AnimeClick = function AnimeClick(anime){
-
-    let nameElement = `anime-${anime}`;
-    let oneElement = window.document.getElementById(nameElement);
-
+window.AnimeClick = function AnimeClick(Anime){
     // Remove o primeiro elemento e o armazena em uma variável
     let firstElement = ArrayPreviewAnime.shift();
 
     // Adiciona o elemento removido ao final da array
     ArrayPreviewAnime.push(firstElement);
 
-
-    ulContainerPlayerAnimes.removeChild(oneElement);
-    ulContainerPlayerAnimes.appendChild(oneElement);
-    bloco1.style.backgroundImage = `url("${encodeURI(ArrayPreviewAnime[0].imagemPC)}")`;
+    loading();
 }
 
-
+function loading() {
+    ulContainerPlayerAnimes.innerHTML = ''
+    geradorPreviewAnime();
+    bloco1.style.backgroundImage = `url("${encodeURI(ArrayPreviewAnime[0].imagemPC)}")`;
+}
 geradorPreviewAnime();
