6ffa7949e2b740a768f6eaea53725dc19ba8e1d3
Ruan-Amorim <150949063+Ruan-Amorim@users.noreply.github.com>
2024-07-12 01:40:20 -0300

    Add files via upload

---
+++ scripts/dados.js
@@ -0,0 +1,43 @@
+
+
+const ArrayPreviewAnime = [
+    {
+        "nome": "Bocchi The Rock",
+        "sinopse": "A história acompanha Hitori Gotou, uma garota sem amigos que está aprendendo a tocar guitarra para se tornar uma estrela do rock, mas é extremamente tímida. Por mais que seu sonho parecesse inalcançável, ela acaba por conhecer Nijika Ijichi, uma baterista que está a procura de uma guitarrista para sua banda, criando assim a oportunidade para Hitori entrar no mundo da música.",
+        "imagemPC": "https://images7.alphacoders.com/129/1296046.jpg",
+        "imagemCL": "https://images7.alphacoders.com/132/1323120.png",
+    },
+    {
+        "nome": "Oshi no Ko",
+        "sinopse": "A história de Oshi no Ko acompanha, inicialmente, Gorou Honda, um ginecologista de um hospital na zona rural do Japão, que é um grande fã da idol Ai Hoshino. Ele fica perplexo quando a artista pausa a carreira, e mais ainda quando ela aparece em seu local de trabalho, grávida.",
+        "imagemPC": "https://moewalls.com/wp-content/uploads/2023/06/ai-hoshino-idol-oshi-no-ko-thumb.jpg",
+        "imagemCL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi_RXg3iilxIifhn4ENHc5glmh8HiTeiTM3A&s",
+    },
+    {
+        "nome": "Youkoso",
+        "sinopse": " A história de um garoto misterioso entra em um colégio de elite com sistema de ranks das classes e alunos. Mas embora não chame muita atenção, ele é muito mais do que parece.",
+        "imagemPC": "https://wallpaper.forfun.com/fetch/8a/8a32b6a93ee5f980398ca1c89c5abdc5.jpeg",
+        "imagemCL": "https://c4.wallpaperflare.com/wallpaper/691/648/922/anime-anime-girls-picture-in-picture-youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-horikita-suzune-hd-wallpaper-preview.jpg",
+    },
+    {
+        "nome": "Isekai de Cheat Skill",
+        "sinopse": "Por muitos anos Tenjo Yuuya sofreu com bullying pesado, até que certo dia encontrou uma porta no porão de seu avó capaz de levá-lo para outro mundo. Recebendo poderes inimagináveis e habilidades consideradas como trapaças, Yuuya passa a se aventurar nesse novo mundo, tentando deixar para trás sua vida de sofrimento e buscando ser alguém mais forte em ambas as realidades.",
+        "imagemPC": "https://wallpapercave.com/wp/wp13231037.jpg",
+        "imagemCL": "https://s1.zerochan.net/Isekai.de.Cheat.Skill.wo.Te.ni.Shita.Ore.wa.Genjitsu.Sekai.wo.mo.Musou.Suru%3A.Level.Up.wa.Jinsei.wo.Kaeta.600.3914522.jpg",
+    },
+    {
+        "nome": "Konosuba",
+        "sinopse": "A história acompanha Kazuma, um jovem que morre de forma humilhante após tentar salvar uma garota. Ele recebe a oportunidade de reencarnar em um novo mundo, decidindo levar a Deusa que debochou dele como “arma”. Porém, para sua surpresa, a Deusa é completamente inútil, tornando a vida de Kazuma mais complicada do que ele gostaria.",
+        "imagemPC": "https://wallpaper.forfun.com/fetch/73/73191a27b4e067af941773b9e0de9732.jpeg",
+        "imagemCL": "https://wallpapercave.com/wp/wp6289793.jpg",
+    },
+    {
+        "nome": "Kenka Dokugaku",
+        "sinopse": "O estudante magrelo do ensino médio, Hobin Yu, é o último cara que você esperaria estrelar em um canal do NewTube, que gira em torno de lutas. Depois de seguir alguns conselhos de um misterioso canal no NewTube, Hobin logo está nocauteando caras mais fortes do que ele e arrecadando mais dinheiro do que jamais poderia ter sonhado.",
+        "imagemPC": "https://pbs.twimg.com/media/GK45V32W0AEY29w.jpg",
+        "imagemCL": "https://pbs.twimg.com/media/GK45V32W0AEY29w.jpg",
+    },
+];
+
+export default ArrayPreviewAnime;
+
---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+
+import ArrayPreviewAnime from "./dados.js";
+
+
+var ulContainerPlayerAnimes = window.document.getElementById("ulContainerPlayerAnimes");
+var bloco1 = window.document.getElementById("bloco1");
+
+function geradorPreviewAnime() {
+    for (let x = 0; x < ArrayPreviewAnime.length; x++) {
+        let li = document.createElement("li");
+        li.id = `anime-${x}`;
+        li.innerHTML = `
+            <h2>${ArrayPreviewAnime[x].nome}</h2>
+            <p>${ArrayPreviewAnime[x].sinopse}</p>`;
+        li.style.backgroundImage = `url("${encodeURI(ArrayPreviewAnime[x].imagemPC)}")`;
+        li.setAttribute('onclick', `AnimeClick(${x})`); 
+    
+        ulContainerPlayerAnimes.appendChild(li);
+    }
+};
+
+window.AnimeClick = function AnimeClick(Anime){
+    // Remove o primeiro elemento e o armazena em uma variável
+    let firstElement = ArrayPreviewAnime.shift();
+
+    // Adiciona o elemento removido ao final da array
+    ArrayPreviewAnime.push(firstElement);
+
+    loading();
+}
+
+function loading() {
+    ulContainerPlayerAnimes.innerHTML = ''
+    geradorPreviewAnime();
+    bloco1.style.backgroundImage = `url("${encodeURI(ArrayPreviewAnime[0].imagemPC)}")`;
+}
+geradorPreviewAnime();
